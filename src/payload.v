fbd99589cf344ddab026fc220eeb25ba640e439a
André Luiz <andreluiz.ddev@gmail.com>
2026-02-18 14:46:08 -0300

    fix: support PKCS#8 RSA keys and strict base64url JWT encoding

--- src/payload.v
+++ src/payload.v
@@ -2,18 +2,30 @@ module jwt
 
 import time
 
-type JsTime = string
+pub type JsTime = i64 | string
 
 pub struct Payload[T] {
 pub:
 	iss ?string @[omitempty]
 	sub ?string @[omitempty]
 	aud ?string @[omitempty]
-	exp JsTime  @[omitempty]
-	iat JsTime  @[omitempty]
+	exp ?JsTime @[omitempty]
+	iat ?JsTime @[omitempty]
 	ext T       @[omitempty]
 }
 
 pub fn (jst JsTime) time() ?time.Time {
-	return time.parse(jst) or { return none }
+	return match jst {
+		string {
+			time.parse(jst) or { return none }
+		}
+		i64 {
+			time.unix(jst)
+		}
+	}
+}
+
+pub fn (p Payload[T]) exp_time() ?time.Time {
+	exp := p.exp or { return none }
+	return exp.time()
 }
